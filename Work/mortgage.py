41acad2d6fd7c1b6e1cd1ef26394413efe76c0d5
trevgall <trevgall@gmail.com>
2023-11-27 21:13:36 +0000

    Exercise 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,13 +7,21 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 month = 1
-additional = 1000
+# extra_payment = 1000
+# extra_payment_start_month = 1
+# extra_payment_stop_month = 13
+
+print('Welcome to the mortgage calculator... ')
+
+extra_payment = input('What should your extra payment be set at? ')
+extra_payment_start_month = input('Which month would you like to start paying extra? ')
+extra_payment_stop_month = input('Which month would you like to stop paying extra? ')
 
 while principal > 0:
-    if month < 13: 
+    if month > int(extra_payment_start_month) and month < int(extra_payment_stop_month): 
         month = month + 1
-        principal = principal * (1+rate/12) - (payment + additional)
-        total_paid = total_paid + payment + additional
+        principal = principal * (1+rate/12) - (payment + int(extra_payment))
+        total_paid = total_paid + payment + int(extra_payment)
     else: 
         month = month + 1
         principal = principal * (1+rate/12) - payment
